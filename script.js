ba8fc415c22bf199991e47b5f0cf394e96d6fe86
mclarabar <mcbp2@aluno.ifal.edu.br>
2025-08-11 11:28:43 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+const cityInput = document.getElementById('city-input');
+const searchBtn = document.getElementById('search-btn');
+const weatherResult = document.getElementById('weather-result');
+const cityName = document.getElementById('city-name');
+const localTime = document.getElementById('local-time');
+const weatherIcon = document.getElementById('weather-icon');
+const temperature = document.getElementById('temperature');
+const condition = document.getElementById('condition');
+const feelsLike = document.getElementById('feels-like');
+const humidity = document.getElementById('humidity');
+const windSpeed = document.getElementById('wind-speed');
+const pressure = document.getElementById('pressure');
+const visibility = document.getElementById('visibility');
+const uvIndex = document.getElementById('uv-index');
+const errorMessage = document.getElementById('error-message');
+
